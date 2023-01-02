8861f7df63e109bb1003a2143160c3f6c7017753
Silvio <sligas3@gmail.com>
2023-01-01 22:37:48 -0300

    agregar config inicial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser');
+const v1WeatherApp = require('./v1/routes/weatherRoutes');
+
+const app = require('../app');
+
+app.use(bodyParser.json());
+app.use('/v1', v1WeatherApp);
+
+module.exports = app;
